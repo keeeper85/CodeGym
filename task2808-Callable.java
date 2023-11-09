b111de2fff3c4d90a2b9d1e38cfff5f35ed8327c
Jakub Raczkowski <122378010+keeeper85@users.noreply.github.com>
2023-11-09 08:49:41 +0100

    Add files via upload

---
+++ task2808-Callable.java
@@ -0,0 +1,67 @@
+package com.codegym.task.task28.task2808;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/* 
+Mastering Callable
+
+Implement the getTask method. It should return a Callable object.
+The Callable object must calculate the sum of the numbers from 1 to the passed number (i), inclusive, and return it as a String.
+The main method is not tested.
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        List<Future<String>> futures = new ArrayList<>();
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        for (int i = 1000; i <= 1010; i++) {
+            futures.add(executor.submit(getTask(i)));
+        }
+
+        futures.add(executor.submit(getTask(10000000)));
+
+        for(Future<String> future : futures) {
+            System.out.println(future.get());
+        }
+
+        executor.shutdown();
+        executor.awaitTermination(10, TimeUnit.SECONDS);
+
+/* Output
+500500
+501501
+502503
+503506
+504510
+505515
+506521
+507528
+508536
+509545
+510555
+50000005000000
+*/
+    }
+
+    public static Callable<String> getTask(final int i) {
+        return new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+
+                long result = i;
+
+                for (long j = 0; j < i; j++) {
+                    result += j;
+                }
+
+
+                return String.valueOf(result);
+            }
+        };
+    }
+}
