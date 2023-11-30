09a3b0d647ef056176e3b65ec7512977c91fbd38
Jakub Raczkowski <122378010+keeeper85@users.noreply.github.com>
2023-11-30 04:58:47 +0100

    Add files via upload

---
+++ task3604-RedBlackTree/Solution.java
@@ -0,0 +1,19 @@
+package com.codegym.task.task36.task3604;
+
+/* 
+Understanding red-black trees
+
+You are given an implementation of a red-black tree.
+But some methods are broken. Analyze the code and fix the errors.
+The main method is not tested. All of the modifiers are correct. Don't change variable or method names.
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        RedBlackTree redBlackTree = new RedBlackTree();
+        redBlackTree.insert(5);
+
+    }
+}
